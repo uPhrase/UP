44bc7ea6b4b2502628edf7f738d2c0ca3496a479
Gunir <134402102+gunir@users.noreply.github.com>
2024-03-01 21:32:35 +0700

    Update uPhrase.py

--- uPhrase.py
+++ uPhrase.py
@@ -8,20 +8,15 @@ f2 = open(os.path.join(os.getcwd(), 'urls.txt'), 'rb').read().splitlines()
 domainlist = b'|'.join(f2)
 domainlist = br'/^.*?' + domainlist + br'.*?/'
 
-result = br'''[Adblock Plus 2.0]
-! Title: uPhrase
-! Description: Filters I use myself but may cause unintended effects of other users. Mostly peculiar YouTube filters.
-! Homepage: https://github.com/gunir/filterlists
-! Expires: 7 days (update frequency)
-! Version: 2 November 2023
-! Syntax: AdBlock
-
-'''
+result = br''''''
 
 for item in f1:
-	k, v = item.split(b'=')
-	rule = domainlist + b'^$doc,replace=/' + k + b'/' + v.replace(br'/', br'\/') + b'/g'
-	result += rule + b'\r\n'
+	try:
+		k, v = item.split(b'=')
+		rule = domainlist + b'^$doc,replace=/' + k + b'/' + v.replace(br'/', br'\/') + b'/g'
+		result += rule + b'\r\n'
+	except:
+		pass
 
 f3 = open(os.path.join(os.getcwd(), 'result.txt'), 'wb')
 f3.write(result)
